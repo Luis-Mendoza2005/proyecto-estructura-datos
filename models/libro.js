7cc01ce6d7cf2df2e742361c2cb613d7bc4250e2
Hermu2515 <andreshernandezmunoz253@gmail.com>
2026-07-24 19:10:44 -0500

    Creacion modelo libro y estructura de nodo

---
+++ models/libro.js
@@ -0,0 +1,14 @@
+//Luis si lees esto voy a poner comentarios en las partes que no se que
+//final del proyecto lo eliminamos.
+class Libro{
+    constructor(codigo, titulo, autor, categoria, estado, imagen){
+        this.codigo = codigo
+        this.titulo = titulo
+        this.autor = autor
+        this.categoria = categoria
+        this.estado = estado
+        this.imagen = imagen
+    }
+}
+
+export default Libro;
---
+++ structures/nodo.js
@@ -0,0 +1,9 @@
+class Nodo{
+    constructor(libro){
+        this.libro = libro;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+export default Nodo;
