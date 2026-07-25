bfeae033419ffb3685461af543b41679bf175251
Hermu2515 <andreshernandezmunoz253@gmail.com>
2026-07-25 12:52:29 -0500

    Arbol Binario: Boton de volver,enter y mejora de diseño

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 import arbol from "./arbol.js";
 
-
+//Inicio de seccin del arbol binario
+//===========================================================
 const botonBuscar = document.getElementById("btnBuscar");
 
 
@@ -34,6 +35,9 @@ function mostrarResultado(libro){
             <h3>
                 Libro no encontrado
             </h3>
+            <p>
+                No existe ningún libro registrado con ese código
+            </p>
         `;
 
         return;
@@ -41,28 +45,43 @@ function mostrarResultado(libro){
 
 
     resultado.innerHTML = `
+        <img src="${libro.imagen}" alt="${libro.titulo}">
 
-        <img 
-            src="${libro.imagen}"
-            width="150"
-        >
+        <h2>${libro.titulo}</h2>
 
-        <h2>
-            ${libro.titulo}
-        </h2>
+        <p><strong>Código:</strong> ${libro.codigo}</p>
 
-        <p>
-            Autor: ${libro.autor}
-        </p>
+        <p><strong>Autor:</strong> ${libro.autor}</p>
 
-        <p>
-            Categoría: ${libro.categoria}
-        </p>
+        <p><strong>Categoría:</strong> ${libro.categoria}</p>
 
-        <p>
-            Estado: ${libro.estado}
-        </p>
+        <p><strong>Estado:</strong> ${libro.estado}</p>
 
     `;
 
 }
+
+
+//Apartado para el boton de volver en la sección del árbol
+const btnVolver = document.getElementById("btnVolver");
+/*Se ubica un if porque hay páginas las cuales no utilizan el
+boton de volver, como es la propia de inicio que index html */
+if(btnVolver){
+    btnVolver.addEventListener("click", ()=>{
+        window.location.href="../index.html";
+    })
+}
+
+
+//Apartado para buscar el libro por medio de enter
+const inputCodigo = document.getElementById("codigoLibro");
+if(inputCodigo){
+    inputCodigo.addEventListener("keydown", (event)=>{
+        if(event.key === "Enter"){
+            document.getElementById("btnBuscar").click();
+        }
+    })
+}
+
+//==========================================================
+//Fin de sección de árbol binario
