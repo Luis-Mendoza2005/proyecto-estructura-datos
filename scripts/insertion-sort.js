0d3ff82272ce3f164c2625295a30d3f4a3e9caf5
Hermu2515 <andreshernandezmunoz253@gmail.com>
2026-07-26 20:48:49 -0500

    Implementación del algoritmo de insertion sort

---
+++ scripts/insertion-sort.js
@@ -0,0 +1,22 @@
+function insertionSort(libros){
+    //Se inicia desde 1 porque se van reocorriendo todos los libros
+    //a partir del segundo.
+    for(let i = 1; i < libros.length; i++){
+        const libroActual = libros[i];
+        //Esta representa el libro con el que vamos a comparar.
+        //Ya que el insertion sort siempre compara con el que tienes a la
+        //izquierda
+        let j = i -1;
+        while (i>=0 && libros[j].titulo.toLowerCase() > libroActual.titulo.toLowerCase()){
+            libros[j+1] = libros[j];
+            j--;
+
+        }
+        //Aqui esta en la posicon de atras, porque primero
+        //se le resta a j, hasta el maximo minimo.
+        libros[j+1] = libroActual;
+    }
+    return libros;
+}
+
+export default insertionSort;
